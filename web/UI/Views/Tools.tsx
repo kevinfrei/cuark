f6736750ab75ebf9b818e756a3a99486036d9f82
Kevin Frei <kevinfrei@hotmail.com>
2026-05-05 17:14:46 -0700

    Actually get the simple-git-hooks registered

--- web/UI/Views/Tools.tsx
+++ web/UI/Views/Tools.tsx
@@ -7,14 +7,10 @@ export function ToolsView(): ReactElement {
   return (
     <div className="tools-view">
       <Expandable separator label="Some Tool" defaultShow>
-        <div>
-          Put some tool thing in here.
-        </div>
+        <div>Put some tool thing in here.</div>
       </Expandable>
       <Expandable separator label="Hidden Tool">
-        <div>
-          Put some less commonly used tool thing in here.
-        </div>
+        <div>Put some less commonly used tool thing in here.</div>
       </Expandable>
     </div>
   );
