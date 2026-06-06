6d5543054fe4975eaa65bae20de3c03c35671a25
Kevin Frei <kevinfrei@hotmail.com>
2026-06-05 22:09:29 -0700

    Updated to module-compatible crow-idl

--- src/ipc/test/json_test.cpp
+++ src/ipc/test/json_test.cpp
@@ -1,13 +1,9 @@
-#include <algorithm>
-#include <iostream>
-#include <optional>
-#include <string>
-#include <vector>
-
 #include <crow.h>
 #include <gtest/gtest.h>
 
-#include "CommonTypes.hpp"
+import std;
+import ts_cpp_idl.Shared;
+import ts_cpp_idl.crow_support;
 
 TEST(CommonTypes, Keys) {
   crow::json::wvalue json_value = to_json(Shared::Keys::Settings);
